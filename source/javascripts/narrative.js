6c88643b878823c9b363ebc5d024e9c42456f1c7
Mark Muhleder <markmuhleder@gmail.com>
2021-12-13 11:17:54 +0000

    Assets

---
+++ source/javascripts/narrative.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("header").classList.add('scroll-shrink');
+    } else {
+        document.getElementById("header").classList.remove('scroll-shrink');
+    }
+}
+
+function showPopUp(event) {
+    event.preventDefault();
+    document.body.classList.add('show-popup');
+}
+
+function hidePopUp() {
+    document.body.classList.remove('show-popup');
+}
+
+document.getElementById('modal').addEventListener('click', hidePopUp);
+
+const triggers = document.getElementsByClassName('trigger-popup');
+for (let trigger of triggers) {
+    trigger.addEventListener('click', showPopUp);
+}
+
